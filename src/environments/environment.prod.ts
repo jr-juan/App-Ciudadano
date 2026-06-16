8a897011bfb03bdbc53552f54ec78bc531a9b085
Juan Cuero <juanromancuero@gmail.com>
2026-06-16 13:55:10 -0500

    Eliminar componentes y archivos no utilizados del contenedor de exploración y las pestañas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
+export const environmentFireBase = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzih4JgSip7dNFvKcsiWEUmemO-BsG_ak",
+    authDomain: "proyecto-angular-dc7a2.firebaseapp.com",
+    projectId: "proyecto-angular-dc7a2",
+    storageBucket: "proyecto-angular-dc7a2.firebasestorage.app",
+    messagingSenderId: "249237705233",
+    appId: "1:249237705233:web:bcae208a71e5220b1ad7b0",
+    measurementId: "G-PBQL9C844Q"
+  }
+};
+
+export const environmentApi = {
+    produccion: true,
+    apiUrl: 'https://apirecoleccion.gonzaloandreslucio.com/api'
+};
+
+export const environmentPerfilId = {
+    produccion: true,
+    perfilId: '321d109f-6396-470a-b30a-ed347f8842c9'
+};
+
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoianJjdWVybyIsImEiOiJjbWkybnV6YjIxY2VjMmpweWczM24wMm5sIn0.LPQ5Q6_iU6jpGbjmgd5NgA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,27 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
+export const environmentFireBase = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzih4JgSip7dNFvKcsiWEUmemO-BsG_ak",
+    authDomain: "proyecto-angular-dc7a2.firebaseapp.com",
+    projectId: "proyecto-angular-dc7a2",
+    storageBucket: "proyecto-angular-dc7a2.firebasestorage.app",
+    messagingSenderId: "249237705233",
+    appId: "1:249237705233:web:bcae208a71e5220b1ad7b0",
+    measurementId: "G-PBQL9C844Q"
+  }
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export const environmentApi = {
+    produccion: false,
+    apiUrl: 'https://apirecoleccion.gonzaloandreslucio.com/api'
+};
+
+export const environmentPerfilId = {
+    produccion: false,
+    perfilId: '321d109f-6396-470a-b30a-ed347f8842c9'
+};
+
+export const environment = {
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoianJjdWVybyIsImEiOiJjbWkybnV6YjIxY2VjMmpweWczM24wMm5sIn0.LPQ5Q6_iU6jpGbjmgd5NgA'
+};
